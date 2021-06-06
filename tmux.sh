6e52589329829c7bd70e3ee0a6eb3f4a89a5ba4a
Joe-mcgee <thulsmans.2994@me.com>
2021-06-06 09:56:35 -0700

    init

---
+++ tmux.sh
@@ -0,0 +1,14 @@
+DIR=${pwd}
+SESSIONNAME=HashedComments
+MEMNONIC=$(grep MEMNONIC .env | cut -d '=' -f 2-)
+BLOCKSPEED=0
+GASLIMIT=8000000
+tmux new-session -s $SESSIONNAME \; \
+	send-keys 'vi ${DIR}' C-m \; \
+	split-window -v \; \
+	split-window -v \; \
+	send-keys "cd web && npm run serve" C-m \; \
+	split-window -h \; \
+	send-keys "ganache-cli -b ${BLOCKSPEED}  --gasLimit=${GASLIMIT}  -m ${MEMNONIC}" C-m \; \
+
+
